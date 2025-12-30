987ca56ef621e7a84bbad7ee165b72e1067bca2a
paulzverev <paulzvereve@gmail.com>
2025-12-30 20:32:18 +0300

    numbers

--- contracts/FirstContract.sol
+++ contracts/FirstContract.sol
@@ -4,20 +4,43 @@ pragma solidity ^0.8.0; // Solidity language version (0.8.something)
 
 contract FirstContract {
 
-    address public me;
-    mapping (address => uint) public transactions;
+    uint public minimum;
 
-    constructor() {
-        me = msg.sender;
+    function demo() public {
+        minimum = type(uint8).min;
+        minimum = type(uint8).max;
     }
 
-    function pay() public payable { // "paybale" means that this function deals with money
-        transactions[msg.sender] = msg.value;
+    uint8 public myVal = 254;
+
+    function increment() public {
+        // myVal += 1; error
+
+        unchecked {
+            myVal += 1; // switches to the limit
+        }
     }
 
-    function withdraw() public {
-        address payable temporaryMe = payable (me);
-        address thisContract = address(this);
-        temporaryMe.transfer(thisContract.balance);
-    }
+    // unsigned integer (only positive)
+    uint public myUint = 42; // or uint256, up to 2**256
+    uint8 public mySmallUint = 2; // up to 2**8 = 256
+
+    // signed integer (+ or -)
+    int public myInt = -42;
+    int8 mySmallInt = -1;
+
+    // bool public myBool = true;
+
+    // function func(bool _inputBool) public {
+    //     bool localBool = false;
+    //     localBool && _inputBool;
+    //     localBool || _inputBool;
+    //     localBool == _inputBool;
+    //     localBool != _inputBool;
+    //     !localBool;
+
+    //     if (inputBool || localBool) {
+
+    //     }
+    // }
 }
