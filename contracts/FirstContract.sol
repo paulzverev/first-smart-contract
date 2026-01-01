e4edbc619af6e5ac8c1ccbc2b3e2edf783807a2e
paulzverev <paulzvereve@gmail.com>
2026-01-01 22:57:57 +0300

    simple transfer

--- contracts/FirstContract.sol
+++ contracts/FirstContract.sol
@@ -1,46 +1,28 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0; // Solidity language version (0.8.something)
+pragma solidity ^0.8.0;
 
 contract FirstContract {
 
-    uint public minimum;
+    address public me;
 
-    function demo() public {
-        minimum = type(uint8).min;
-        minimum = type(uint8).max;
+    constructor() {
+        me = msg.sender;
     }
 
-    uint8 public myVal = 254;
-
-    function increment() public {
-        // myVal += 1; error
-
-        unchecked {
-            myVal += 1; // switches to the limit
-        }
+    function pay() public payable {
+        // the balance of the contract will be increased automatically
     }
 
-    // unsigned integer (only positive)
-    uint public myUint = 42; // or uint256, up to 2**256
-    uint8 public mySmallUint = 2; // up to 2**8 = 256
+    function withdraw() public {
+        address payable payableMe = payable(me);
+        address thisContract = address(this);
+        uint contractBalance = thisContract.balance;
 
-    // signed integer (+ or -)
-    int public myInt = -42;
-    int8 mySmallInt = -1;
+        payableMe.transfer(contractBalance);
+    }
 
-    // bool public myBool = true;
-
-    // function func(bool _inputBool) public {
-    //     bool localBool = false;
-    //     localBool && _inputBool;
-    //     localBool || _inputBool;
-    //     localBool == _inputBool;
-    //     localBool != _inputBool;
-    //     !localBool;
-
-    //     if (inputBool || localBool) {
-
-    //     }
-    // }
+    function getCurrentBalance() public view returns(uint) {
+        return me.balance;
+    }
 }
